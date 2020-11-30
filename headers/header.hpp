4d244ce9539af0ff9cea7b1d3f2261df01bffc99
mocurin <elleyzzzzz@gmail.com>
2020-11-30 18:29:24 +0300

    Implemented bubble sort

--- headers/header.hpp
+++ headers/header.hpp
@@ -5,4 +5,28 @@
 #ifndef ADS_HEADER_HPP
 #define ADS_HEADER_HPP
 
+#include <iterator>
+
+template <class BidirectionalIt, class Compare>
+void bubble_sort(BidirectionalIt first, BidirectionalIt last, Compare comp) {
+    // check if `last - first == 0 -> seq is empty`
+    if (first == last) return;
+    // can also be ++first, but i'd wish to change name
+    auto init_it = std::next(first);
+    // check if `last - first != 1 -> seq is one element long`
+    // then check if
+    while (init_it != last) {
+        auto changed = false;
+        for (auto it = init_it; it < last; ++it) {
+            auto prev_it = std::prev(it);
+            if (comp(*it, *prev_it)) {
+                std::iter_swap(it, prev_it);
+                changed = true;
+            }
+        }
+        if (!changed) return;
+        --last;
+    }
+}
+
 #endif //ADS_HEADER_HPP
--- tests/test.cpp
+++ tests/test.cpp
@@ -2,10 +2,25 @@
 // Created by mocurin on 30.11.2020.
 //
 
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <random>
+#include <vector>
+
 #include "gtest/gtest.h"
 
 #include "header.hpp"
 
-TEST(dummy_test, dummy) {
-    EXPECT_TRUE(true);
+TEST(BubbleSortTest, Test1) {
+    std::vector<int> victim(1000);
+    std::iota(victim.begin(), victim.end(), 0);
+    std::vector<int> target(victim);
+    std::mt19937 rgen(42);
+    std::shuffle(victim.begin(), victim.end(), rgen);
+
+    bubble_sort(victim.begin(), victim.end(), std::less<>());
+    for (size_t i = 0; i < target.size(); ++i) {
+        EXPECT_EQ(victim[i], target[i]);
+    }
 }
